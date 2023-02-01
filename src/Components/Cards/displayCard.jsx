10c1e130e7a7c527bf08be233d599be8a5b42af1
Piyush-Bhagwat <piyushbhagwat5@gmail.com>
2023-02-01 16:20:17 +0530

    added ui element for Edit button

--- src/Components/Cards/displayCard.jsx
+++ src/Components/Cards/displayCard.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 
-export default function DisplayCard({ content, tag, id, onDelete }) {
+export default function DisplayCard({ content, tag, id, onDelete, onEdit }) {
     return (
         <div className="card">
             <p className="content">{content}</p>
 
-            
             <div className="tagBtn">
-                {(tag!=="")? <p className="tag">{tag}</p>: <div></div>}
-                <button className="delBtn" onClick={()=> onDelete(id)}>
-                    <i className="fa-solid fa-trash"></i>
-                </button>
+                {tag !== "" ? <p className="tag">{tag}</p> : <div></div>}
+
+                <div className="controls">
+                    <button className="delBtn" onClick={() => onEdit(id)}>
+                        <i class="fa-solid fa-pencil"></i>
+                    </button>
+                    <button className="delBtn" onClick={() => onDelete(id)}>
+                        <i className="fa-solid fa-trash"></i>
+                    </button>
+                </div>
             </div>
         </div>
     );
